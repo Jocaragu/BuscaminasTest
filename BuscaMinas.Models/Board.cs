8344b82a743adce8bfa4386603f4e884aba8947f
Jocaragu <jo_ca_ra_gu@hotmail.com>
2022-09-12 21:18:08 -0700

    1

---
+++ BuscaMinas.Models/Board.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuscaMinas.Models
+{
+    public class Board
+    {
+        public List<Cell> BoardCells { get; set; }
+        public Board(int width, int height)
+        {
+            BoardCells = BoardingTheCells(width, height);
+        }
+        public List<Cell> BoardingTheCells(int width, int height)
+        {
+            var boardedCells = new List<Cell>();
+            for(int i = 0; i < width; i++)
+            {
+                for(int j = 0; j < height; j++)
+                {
+                    boardedCells.Add(new Cell(i+1, j+1));
+                }
+            }
+            return boardedCells;
+        }
+    }
+}
