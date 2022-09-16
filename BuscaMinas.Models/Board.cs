a6de858ade764ef5f77c8bfd7002e2488a985445
Jocaragu <jo_ca_ra_gu@hotmail.com>
2022-09-15 17:18:14 -0700

    Added a custom event that allows to track which individual cell is being revealed, it will be used to build upon other game and board functionalities.

--- BuscaMinas.Models/Board.cs
+++ BuscaMinas.Models/Board.cs
@@ -1,29 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static BuscaMinas.Models.Cell;
 
 namespace BuscaMinas.Models
 {
     public class Board
     {
-        public List<Cell> BoardCells { get; set; }
+        //public event EventHandler<string> GameCellsAddingNewEvent;
+
+        public BindingList<Cell> GameCells { get; set; }
+        //public List<Cell> BoardCells { get; set; }
         public Board(int width, int height)
         {
-            BoardCells = BoardingTheCells(width, height);
+            //BoardCells = BoardingTheCells(width, height);
+            GameCells = BoardingGameCells(width,height);
         }
-        public List<Cell> BoardingTheCells(int width, int height)
+
+        //public List<Cell> BoardingTheCells(int width, int height)
+        //{
+        //    var boardedCells = new List<Cell>();
+        //    for (int i = 0; i < width; i++)
+        //    {
+        //        for (int j = 0; j < height; j++)
+        //        {
+        //            boardedCells.Add(new Cell(i + 1, j + 1));
+        //        }
+        //    }
+        //    return boardedCells;
+        //}
+        public BindingList<Cell> BoardingGameCells(int width, int height)
         {
-            var boardedCells = new List<Cell>();
-            for(int i = 0; i < width; i++)
+            var boardingGameCells = new BindingList<Cell>();
+            for (int i = 0; i < width; i++)
             {
-                for(int j = 0; j < height; j++)
+                for (int j = 0; j < height; j++)
                 {
-                    boardedCells.Add(new Cell(i+1, j+1));
+                    Cell cell = new(i + 1, j + 1);
+                    cell.CellWasRevealed += Cell_CellWasRevealed;
+                    boardingGameCells.Add(cell);
                 }
             }
-            return boardedCells;
+            //boardingGameCells.ListChanged += BoardingGameCells_ListChanged;
+            return boardingGameCells;
         }
+
+        //private void BoardingGameCells_ListChanged(object? sender, ListChangedEventArgs e)
+        //{
+        //    Console.WriteLine("CHANGE CHANGE CHANGE");
+        //}
+        private void Cell_CellWasRevealed(object? sender, CellWasRevealedArgs e)
+        {
+            var xArg = e.XRevelationValue;
+            var yArg = e.YRevelationValue;
+            Console.WriteLine("REVELATION REVELATION REVELATION");
+        }
+
+        //public void MineTheCells(Cell seed)
+        //{
+        //    for (int i = 0; i < BoardCells.Count; i++)
+        //    {
+        //        var cell = BoardCells[i];
+        //        if (seed.XValue != cell.XValue && seed.YValue != cell.YValue)
+        //        {
+        //            cell.Mined = true;
+        //        }
+        //    }
+        //}
     }
 }
--- BuscaMinas.Models/Cell.cs
+++ BuscaMinas.Models/Cell.cs
@@ -1,31 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BuscaMinas.Models
 {
-    public class Cell
+    public class Cell// : INotifyPropertyChanged
     {
         public int Id { get; set; }
         public int XValue { get; set; }
         public int YValue { get; set; }
         public int NearbyMines { get; set; }
+        //private bool _revealed = false;
+        //public bool Revealed 
+        //{ 
+        //    get 
+        //    {
+        //        return _revealed; 
+        //    }
+        //    set 
+        //    { 
+        //        if(value != _revealed)
+        //        {
+        //            _revealed = value;
+        //            NotifyPropertyChanged();
+        //        }
+        //    } 
+        //}
         public bool Revealed { get; set; }
         public bool Mined { get; set; }
         public bool Flagged { get; set; }
         public bool Questioned { get; set; }
+        public CellWasRevealedArgs RevealedArgs { get; set; }
         public Cell(int xValue, int yValue)
         {
             XValue = xValue;
             YValue = yValue;
+            RevealedArgs = new(xValue, yValue);
         }
         public void LeftClick()
         {
             if(!Revealed && !Flagged)
             {
                 Revealed = true;
+                CellWasRevealed?.Invoke(this, RevealedArgs);
             }
         }
         public void RightClick()
@@ -34,6 +55,23 @@ namespace BuscaMinas.Models
             {
                 Flagged = !Flagged;
             }
+        }
+        public event EventHandler<CellWasRevealedArgs>? CellWasRevealed;
+        //public event PropertyChangedEventHandler? PropertyChanged;
+        //private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+
+    }
+    public class CellWasRevealedArgs : EventArgs
+    {
+        public int XRevelationValue { get; set; }
+        public int YRevelationValue { get; set; }
+        public CellWasRevealedArgs(int xEventValue, int yEventValue)
+        {
+            XRevelationValue = xEventValue;
+            YRevelationValue = yEventValue;
         }
     }
 }
